e9ed0b21a10387e330ffe9ad3bff805825430037
r-alx <kycoin890@gmail.com>
2024-01-30 17:11:39 +0700

    the pro

---
+++ string_method.py
@@ -0,0 +1,4 @@
+no = int(input("enter the num that is the largest:"))
+on = int(input("enter the num that is the smaller than the first nưme got:"))
+last_digit = no - on
+print("The value of the num is:",last_digit)
