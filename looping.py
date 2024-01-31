63d1821d593b93af87cde2b7cf1bb0901c4891cf
r-alx <kycoin890@gmail.com>
2024-01-31 20:36:49 +0700

    I will be rich soon

---
+++ looping.py
@@ -0,0 +1,5 @@
+for f in range(87):
+    if f % 2 == 0:
+        print(f,"<<<!the num is even!>>>")
+    else:
+        print(f,"<<<!the num is odd!>>>")
---
+++ range.py
@@ -0,0 +1,4 @@
+num = 2
+while num <= 10:
+    print("2 x",num,"=",1 * num)
+    num = num + 1
