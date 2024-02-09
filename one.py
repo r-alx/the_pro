20b4007398f016358d50624f8f596e852787ffb6
r-alx <kycoin890@gmail.com>
2024-02-09 11:45:25 +0700

    lambo

---
+++ one.py
@@ -0,0 +1,5 @@
+for l in range(1036):
+    if l % 2 == 0:
+        print(l,"<<<the num is even>>>")
+    else:
+        print(l,"<<<the num is odd>>>")
---
+++ two.py
@@ -0,0 +1,4 @@
+num = 3
+while num <= 12:
+    print("3 x",num,"=",3 * num)
+    num = num + 1
